a5e6dd6fa89c9c133799aeee26a87db08a28a50c
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-07-09 01:24:03 +0530

    mlab uri add

--- app/User.js
+++ app/User.js
@@ -1,6 +1,6 @@
 var mongoose = require ("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://pjprateekjoshi:shinchan01@ds131711.mlab.com:31711/newdb" || "mongodb://localhost/newdb");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/newdb" || "mongodb://pjprateekjoshi:shinchan01@ds131711.mlab.com:31711/newdb");
 
 var userSchema = new mongoose.Schema({
   name: String,
