689376e72fb6c94c611e8297aa0b4f5e0a5630c6
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-07-15 03:21:47 +0530

    Update User.js

--- app/User.js
+++ app/User.js
@@ -1,5 +1,5 @@
 var mongoose = require ("mongoose");
-mongoose.connect("mongodb://pjprateekjoshi:shinchan01@ds131711.mlab.com:31711/newdb");
+mongoose.connect("mongodb://pjprateekjoshi:password@ds131711.mlab.com:31711/newdb");
 
 var userSchema = new mongoose.Schema({
   name: String,
